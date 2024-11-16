0f22c178df26a77fe10da64a632706b77131d277
webtesting22 <webtesting2232@gmail.com>
2024-11-16 20:20:38 +0530

    product Add

--- src/Components/MainComponent/ProductsHomePage/ProductData.jsx
+++ src/Components/MainComponent/ProductsHomePage/ProductData.jsx
@@ -35,6 +35,29 @@ const ProductData = [
             "https://images.unsplash.com/photo-1700196804383-778f6535a27a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "https://images.unsplash.com/photo-1700196798611-d27af6d9c566?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         ]
-    }
+    },
+    {
+        title: "Blanket",
+        image: [
+            "https://images.unsplash.com/photo-1641644785726-26ec66bfce73?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1715475160658-39c34218fb84?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        ]
+    },
+    {
+        title: "Wrist Watch",
+        image: [
+            "https://images.unsplash.com/photo-1657235895095-e043ce2ebf41?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1587466412525-87497b34fc88?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+        ]
+    },
+    {
+        title: "Books",
+        image: [
+            "https://images.unsplash.com/photo-1685478237208-3035207b8a1f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+            "https://images.unsplash.com/photo-1705721357357-ab87523248f7?q=80&w=2153&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        ]
+    },
 ]
 export default ProductData;
